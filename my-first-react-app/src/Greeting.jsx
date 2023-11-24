20794527d33b6dda1013a470511030075377371b
Denis Kolokolov <dennis.kolokolov@gmail.com>
2023-11-24 15:32:46 +0100

    What Is JSX?

--- my-first-react-app/src/Greeting.jsx
+++ my-first-react-app/src/Greeting.jsx
@@ -1,5 +1,9 @@
 function Greeting() {
-  return <h1>Hello, this is your first React App!</h1>;
+  return (
+    <>
+      <h1>Hello, this is your first React App!</h1>;
+    </>
+  )
 }
 
 export default Greeting
--- my-first-react-app/src/main.jsx
+++ my-first-react-app/src/main.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Greeting from './Greeting.jsx'
+import Syntax from './Syntax.jsx'
 import { favFood as FavFood, favCloths as FavCloths } from './Favorites.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Greeting />
+    <Syntax />
     <FavFood />
     <FavCloths />
   </React.StrictMode>,

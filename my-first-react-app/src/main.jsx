ebc8bdb0419c411b5db4c0db1d8d9bfe0af76ded
Denis Kolokolov <dennis.kolokolov@gmail.com>
2023-12-06 15:31:17 +0100

    cleaning up

--- my-first-react-app/src/main.jsx
+++ my-first-react-app/src/main.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Greeting from './Greeting.jsx'
-import Syntax from './Syntax.jsx'
-import List from './List.jsx'
-import { favFood as FavFood, favCloths as FavCloths } from './Favorites.jsx'
+// import Syntax from './Syntax.jsx'
+// import List from './List.jsx'
+// import { favFood as FavFood, favCloths as FavCloths } from './Favorites.jsx'
 import './index.css'
 
 const animals = ["Lion", "Cow", "Snake", "Lizard", "Cat", "Dog"];
@@ -12,10 +12,11 @@ const animals = ["Lion", "Cow", "Snake", "Lizard", "Cat", "Dog"];
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Greeting />
+    {/* <App /> */}
     {/* <Syntax />
     <FavFood />
     <FavCloths /> */}
-    <h1>Animals: </h1>
-    <List animals={animals} />
+    {/* <h1>Animals: </h1>
+    <List animals={animals} /> */}
   </React.StrictMode>,
 )
